0b541be9778b8ee9b6141d86176b240e0a84c8bb
PAREDES ARENAS MAXIMILIANO DANIEL <m.paredesarenas@uandresbello.edu>
2023-02-12 16:51:17 -0300

    cambios

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,22 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import "./footer-style.css";
+
+interface Props {}
+
+const Footer: React.FC<Props> = () => {
+  return (
+    <div className="footer">
+      <a href="https://github.com/unknownMaaax">
+        <FaGithub size={32} className="icon" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/maxparedesarenas/"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin size={32} className="icon" />
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
