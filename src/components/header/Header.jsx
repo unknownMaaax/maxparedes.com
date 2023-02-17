ad9668309e1fefbdad3ff358cb7dd69ed38e3b54
PAREDES ARENAS MAXIMILIANO DANIEL <m.paredesarenas@uandresbello.edu>
2023-02-17 15:03:17 -0300

    nuevo

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState } from "react";
+
+export const Header = () => {
+  const [sidebar, setSidebar] = useState(false);
+  return <div>a</div>;
+};
+export default Header;
